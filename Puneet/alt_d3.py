1cc40c6517fa62a1bfb5e193ce68b100d9a32894
pk <pkumar@avkauto.com>
2024-01-07 17:06:01 +0530

    added comments to understand the soln

--- Puneet/alt_d3.py
+++ Puneet/alt_d3.py
@@ -3,19 +3,23 @@ grid = open('/Users/pk/proj/adventOfCode/Puneet/d3_sample_input.txt').read().spl
 
 cs = set()
 
+#find the symbols and the starting top left row, col of the digits surrounding the symbol
 for r, row in enumerate(grid):
     for c, ch in enumerate(row):
+        # print('r : ' + str(r) + ' c : ' + str(c) + ' ch : ' + str(ch))
         if ch.isdigit() or ch == ".":
             continue
         for dr in range(r - 1, r + 2):
+            print('dr : ' + str(dr))
             for dc in range(c - 1, c + 2):
+                # print('dc : ' + str(dc))
                 if dr < 0 or dr >= len(grid) or dc < 0 or dc >= len(grid[dr]) or not grid[dr][dc].isdigit():
                     continue
                 while dc > 0 and grid[dr][dc - 1].isdigit():
+                    # print('dc : ' + str(dc) + ' grid[dr][dc - 1] : ' + str(grid[dr][dc - 1]) + ' dr : ' + str(dr))
                     dc -= 1
                 cs.add((dr, dc))
-
-print(cs)
+                # print('cs : ' + str(cs))
 
 ns = []
 
